0ce8ae71de191c38b0cda074e34ffb61666a708d
jamesvrooney <jamesvrooney@hotmail.com>
2018-01-01 09:49:27 +0000

    Implemented service methods

--- src/main/java/com/rooney/james/PropertyRecordServiceImpl.java
+++ src/main/java/com/rooney/james/PropertyRecordServiceImpl.java
@@ -1,6 +1,11 @@
 package com.rooney.james;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Created by jamesvrooney on 31/12/2017.
@@ -8,16 +13,41 @@ import java.util.List;
 public class PropertyRecordServiceImpl implements PropertyRecordService {
     @Override
     public Double getAveragePriceInPostcodeOuter(List<PropertyRecord> propertyRecords, String outwardCode) {
-        return null;
+        OptionalDouble average = propertyRecords.stream()
+                .filter(byOutwardCode(outwardCode))
+                .mapToDouble(PropertyRecord::getPrice)
+                .average();
+
+        return average.getAsDouble();
     }
 
     @Override
     public List<PropertyRecord> getTopNPercentMostExpensiveProperties(List<PropertyRecord> propertyRecords, Double percent) {
-        return null;
+        double numberOfRecords = propertyRecords.size();
+        double percentage = numberOfRecords/percent;
+
+        List<PropertyRecord> propertyRecordsOrderedByPriceDesc = propertyRecords.stream()
+                .sorted(Comparator.comparingDouble(PropertyRecord::getPrice).reversed())
+                .collect(Collectors.toList());
+
+        int endIndex = (int)Math.round(percentage);
+        List<PropertyRecord> topNPercentMostExpensive = propertyRecordsOrderedByPriceDesc.subList(0, endIndex);
+
+        return topNPercentMostExpensive;
     }
 
     @Override
     public long getDifferenceInAveragePricesBetweenTypes(List<PropertyRecord> propertyRecords, List<PropertyType> propertyTypes) {
-        return 0;
+        Map<PropertyType, Double> averagePricePerType = propertyRecords.stream()
+                .filter(propertyRecord -> propertyTypes.contains(propertyRecord.getPropertyType()))
+                .collect(Collectors.groupingBy(PropertyRecord::getPropertyType, Collectors.averagingInt(PropertyRecord::getPrice)));
+
+        Double difference = averagePricePerType.get(propertyTypes.get(0)) - averagePricePerType.get(propertyTypes.get(1));
+
+        return Math.round(Math.abs(difference));
+    }
+
+    private static Predicate<PropertyRecord> byOutwardCode(String outwardCode) {
+        return propertyRecord -> propertyRecord.getPostcode().getOutwardCode().equals(outwardCode);
     }
 }
